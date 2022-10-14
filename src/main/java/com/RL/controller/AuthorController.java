21627b1a187fbbcefb745adb46c18ab0d504635b
Mustafa Budak <mbudak@gmail.com>
2022-10-14 22:41:48 +0300

    ilk commit

--- src/main/java/com/RL/controller/AuthorController.java
+++ src/main/java/com/RL/controller/AuthorController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @RequestMapping("/author")
 @AllArgsConstructor
 public class AuthorController {
-    //..Mustafa
+    //..Mustafa Budak
     private final AuthorService authorService;
 
     @PostMapping
