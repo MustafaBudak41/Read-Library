f1baf7d85951079252a65de99e51ee7313376f0a
cener78 <94464442+cener78@users.noreply.github.com>
2022-10-14 16:22:26 +0200

    loan create methods updated

--- src/main/java/com/RL/repository/LoanRepository.java
+++ src/main/java/com/RL/repository/LoanRepository.java
@@ -1,7 +1,5 @@
 package com.RL.repository;
 
-
-
 import com.RL.domain.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,13 +10,7 @@ import java.util.List;
 @Repository
 public interface LoanRepository extends JpaRepository<Loan,Long> {
 
-
-   // @Query("select l from Loan l " +
-   //         "left join fetch l.user u " +
-   //         "left join fetch u.id uid " +
-   //         "where l.expireDate<?2 and uid=?1")
-   // List<Loan> findExpiredLoansFindByUserId(Long id,LocalDateTime time);
     @Query("SELECT l from Loan l " +
-            "where l.expireDate<current_date")
-    List<Loan>findExpiredLoansBy();
+            "where l.expireDate<current_date and l.userId=?1")
+    List<Loan>findExpiredLoansBy(Long userId);
 }
