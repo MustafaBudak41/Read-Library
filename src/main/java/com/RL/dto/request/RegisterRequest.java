f3481b103d2debde7583c00ecb4edb24a7715843
muzafferyenice <muzafferyenicet@gmail.com>
2022-10-15 00:06:37 +0300

    Create Methods added

--- src/main/java/com/RL/dto/request/RegisterRequest.java
+++ src/main/java/com/RL/dto/request/RegisterRequest.java
@@ -1,10 +1,56 @@
 package com.RL.dto.request;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Date;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
 
 
+        @NotNull(message="Please provide first name")
+        @Size(min=2, max=30,message="Your first name '${validatedValue}' must be between {min} and {max} chars long")
+        private String firstName;
 
+        @NotNull(message="Please provide last name")
+        @Size(min=2, max=30,message="Your last name '${validatedValue}' must be between {min} and {max} chars long")
+        private String lastName;
 
+        @NotNull(message="Please provide address")
+        @Size(min=10, max=100,message="Address '${validatedValue}' must be between {min} and {max} chars long")
+        private String address;
+
+        @NotNull(message="Please provide phone number")
+        @Size(min=12, max=12,message="Phone number '${validatedValue}' must be {max} chars long")
+        @Pattern(regexp = "^\\d{3}-\\d{3}-\\d{4}$",message = "Please provide valid phone number")
+        private String phone;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        @NotNull(message="Please provide birthDate")
+        private Date birthDate;
+
+        @Email(message = "Please provide valid email")
+        @NotNull(message="Please provide email")
+        @Size(min=10, max=80,message="Email '${validatedValue}' must be between {min} and {max} chars long")
+        private String email;
+
+        @NotNull(message="Please provide password")
+        private String password;
+
+        @NotNull(message="Please provide user builtIn")
+        @Column(nullable = false)
+        private Boolean builtIn = false;
 
 }
