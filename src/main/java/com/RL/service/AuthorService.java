2a3b730d96ba7acba2f1ecdc5b19fec8dbdb15a5
Mustafa Budak <mbudak@gmail.com>
2022-10-14 17:29:36 +0300

    Test and Controller created

--- src/main/java/com/RL/service/AuthorService.java
+++ src/main/java/com/RL/service/AuthorService.java
@@ -1,17 +1,32 @@
 package com.RL.service;
 
-
 import com.RL.domain.Author;
-
+import com.RL.exception.ResourceNotFoundException;
+import com.RL.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
+public class AuthorService {
+    @Autowired
+    private AuthorRepository repository;
+    public void createAuthor(Author author){
+        repository.save(author);
+    }
 
-public interface AuthorService {
+    public List<Author> getAll(){
+        return repository.findAll();
+    }
 
-    void createAuthor(Author author);
+    public Author findById(Long id){
+        return repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Author not found"));
+    }
 
-    List<Author> getAll();
 
-    Author findById(Long id);
+    public void updateAuthor(Long id,Author author) {
+        repository.deleteById(id);
+        repository.save(author);
+    };
 
 }
