822afc136301800fb02dd559f3f4bad0e27eb233
Mustafa Budak <mbudak@gmail.com>
2022-10-19 14:40:25 +0300

    Authors EndPoints Created

--- src/main/java/com/RL/controller/AuthorController.java
+++ src/main/java/com/RL/controller/AuthorController.java
@@ -1,17 +1,18 @@
 package com.RL.controller;
 
-
-
 import com.RL.domain.Author;
 import com.RL.domain.User;
 import com.RL.dto.AuthorDTO;
+import com.RL.dto.mapper.AuthorMapper;
 import com.RL.dto.request.RegisterRequest;
 import com.RL.service.AuthorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
@@ -21,10 +22,10 @@ import java.util.Map;
 @RequestMapping
 @AllArgsConstructor
 public class AuthorController {
-    //..Mustafa Budak
     private final AuthorService authorService;
 
-    @PostMapping("/author")
+    @PostMapping("/authors")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Map<String,String>> createAuthor(@Valid @RequestBody AuthorDTO authorDTO){
 
      Author newAuthor  = authorService.createAuthor(authorDTO);
@@ -34,41 +35,38 @@ public class AuthorController {
         return new ResponseEntity<>(map,HttpStatus.CREATED);
     }
 
-
-
-    @GetMapping("/author")
+    @GetMapping("/authors")
     public ResponseEntity<List<AuthorDTO>> getAll(){
-        List<AuthorDTO> author=authorService.getAll();
-        return ResponseEntity.ok(author);
+        List<AuthorDTO> authors=authorService.getAll();
+        return ResponseEntity.ok(authors);
     }
 
-    @GetMapping("/author/{id}")
-    public ResponseEntity<AuthorDTO> findById(@PathVariable("id") Long id){
-        AuthorDTO author= authorService.findById(id);
-        return  ResponseEntity.ok(author);
-    }
-
-
-
-//    @PatchMapping
-//    @PreAuthorize("hasRole('MEMBER')")
-//    public ResponseEntity<UserDTO> updateUser(HttpServletRequest request, @Valid @RequestBody UserUpdateRequest userUpdateRequest){
-//        Long id = (Long) request.getAttribute("id");
-//        UserDTO userDTO = userService.updateUser(id,userUpdateRequest);
-//        return ResponseEntity.ok(userDTO);
-//    }
-
-
-    @PutMapping("/author/{id}")
-    public ResponseEntity<Map<String,String>> updateAuthor(@RequestParam("id") Long id, @Valid @RequestBody AuthorDTO authorDTO){
-
-        authorService.updateAuthor(id,authorDTO);
+    @GetMapping("/authors/{id}")
+    public ResponseEntity<Map<String,String>>  findById(@PathVariable("id") Long id){
+        AuthorDTO authorDTO= authorService.findById(id);
         Map<String,String> map=new HashMap<>();
-        map.put("id : ", id.toString());
-        map.put("name : ", authorDTO.getName());
+        map.put("id : ", authorDTO.getId().toString());
+        map.put("name : ",authorDTO.getName());
         return new ResponseEntity<>(map,HttpStatus.OK);
-
     }
 
+    @PatchMapping("/authors/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String,String>> updateAuthor(@PathVariable("id") Long id, @Valid @RequestBody Author author){
+        Author author1 = authorService.updateAuthor(id,author);
+        Map<String,String> map=new HashMap<>();
+        map.put("id : ", author1.getId().toString());
+        map.put("name : ",author1.getName());
+        return new ResponseEntity<>(map,HttpStatus.CREATED);
+    }
+    @DeleteMapping("/authors/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String,String>> deleteById(@PathVariable("id") Long id){
+       Author author= authorService.deleteById(id);
+        Map<String,String> map=new HashMap<>();
+        map.put("id : ", author.getId().toString());
+        map.put("name : ",author.getName());
+        return new ResponseEntity<>(map,HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/com/RL/service/AuthorService.java
+++ src/main/java/com/RL/service/AuthorService.java
@@ -1,27 +1,16 @@
 package com.RL.service;
 
 import com.RL.domain.Author;
-import com.RL.domain.Role;
-import com.RL.domain.User;
-import com.RL.domain.enums.RoleType;
 import com.RL.dto.AuthorDTO;
 import com.RL.dto.mapper.AuthorMapper;
-import com.RL.dto.request.RegisterRequest;
 import com.RL.exception.BadRequestException;
-import com.RL.exception.ConflictException;
 import com.RL.exception.ResourceNotFoundException;
 import com.RL.exception.message.ErrorMessage;
 import com.RL.repository.AuthorRepository;
 import com.RL.repository.BookRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @AllArgsConstructor
 @Service
@@ -37,61 +26,35 @@ public class AuthorService {
         return author;
     }
 
-    @Transactional(readOnly = true)
+
     public List<AuthorDTO> getAll() {
         List<Author> authorList = repository.findAll();
         return authorMapper.map(authorList);
     }
 
-    @Transactional(readOnly = true)
+
     public AuthorDTO findById(Long id) {
         Author author = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Author not found"));
         return authorMapper.authorToAuthorDTO(author);
     }
 
 
-//    public UserDTO updateUser(Long id, UserUpdateRequest request) {
-//        boolean existEmail= userRepository.existsByEmail(request.getEmail());
-//
-//        User user = userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException(ErrorMessage.ROLE_NOT_FOUND_MESSAGE));
-//
-//        if(user.getBuiltIn()){
-//            throw new BadRequestException("Not permitted");
-//        }
-//
-//        if (existEmail && !request.getEmail().equals(user.getEmail())){
-//            throw new ConflictException(String.format(ErrorMessage.EMAIL_ALREADY_EXIST_MESSAGE,user.getEmail()));
-//        }
-//
-//        User updateUser = userMapper.updateUserBySelf(request);
-//        updateUser.setCreateDate(user.getCreateDate());
-//        updateUser.setBuiltIn(user.getBuiltIn());
-//        updateUser.setScore(user.getScore());
-//        updateUser.setPassword(user.getPassword());
-//        updateUser.setId(user.getId());
-//        updateUser.setRoles(user.getRoles());
-//
-//        userRepository.save(updateUser);
-//        return userMapper.userToUserDTO(updateUser);
-//    }
-
-
-    @Transactional
-   public AuthorDTO updateAuthor(Long id, AuthorDTO authorDTO) {
+   public Author updateAuthor(Long id, Author author) {
         Author foundAuthor = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Author not found"));
-
         if(foundAuthor.getBuiltIn()) {
             throw new BadRequestException(ErrorMessage.NOT_PERMITTED_METHOD_MESSAGE);
         }
+        foundAuthor.setId(id);
+        foundAuthor.setName(author.getName());
+        foundAuthor.setBuiltIn(author.getBuiltIn());
+        repository.save(foundAuthor);
+       return foundAuthor;
+    }
 
-        Author author = authorMapper.authorDTOToAuthor(authorDTO);
-
-        author.setId(foundAuthor.getId());
-        author.setName(authorDTO.getName());
-
-        repository.save(author);
-       return authorMapper.authorToAuthorDTO(author);
-
+    public Author deleteById(Long id) {
+        Author author = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Author not found"));
+        repository.deleteById(id);
+        return author;
     }
 
 }
